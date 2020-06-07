8e06ab41fd752f55bff75c29b4628172eed09613
olegvetlug <65702094+olegvetlug@users.noreply.github.com>
2020-06-07 17:54:18 +0300

    вывод квадратной матрицы и умножение матриц

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,17 +1,38 @@
 package ru.synergy;
 
 public class Main {
-
             public static void main(String[] args) {
                 int[][] squarMatrix = {{1, 2},
                                        {3, 4}};
-                for (int i = 0; i < squarMatrix.length; i++) {
-                    for (int j = 0; j < squarMatrix[i].length; j++) {
-                        System.out.print(squarMatrix[i][j] + " ");
+                int [][] twoDimArray1 = {{1,0,0,0},{0,1,0,0},{0,0,0,0}};//объявляем две матрицы
+                int[][] twoDimArray2 = {{1,2,3},{1,1,1},{0,0,0},{2,1,0}};
+//процесс умножения матриц
+                int[][]twoDimArray3 = new int [twoDimArray1.length][twoDimArray2[0].length];
+                int k;
+                for (int i = 0; i<twoDimArray3[0].length; i++)
+                    for (int j=0; j<twoDimArray3.length; j++)
+                        for (k=0; k<twoDimArray1[0].length; k++)
+                            twoDimArray3[i][j] = twoDimArray3[i][j] + twoDimArray1[i][k] * twoDimArray2[k][j];
+
+//вывод на экран
+                for (int i = 0; i < twoDimArray3.length; i++) {
+                    for (int j = 0; j < twoDimArray3[0].length; j++) {
+                        System.out.print(" " + twoDimArray3[i][j] + " ");
+                    }
+                    System.out.println();
+                for (int i1 = 0; i1 < squarMatrix.length; i1++) {
+                    for (int j = 0; j < squarMatrix[i1].length; j++) {
+                        System.out.print(squarMatrix[i1][j] + " ");
                     }
                     System.out.println("");
                 }
             }
         }
+
+
+
+
+        }
+
 
 
